a923bb4c92e250c20be112f5d2694e1a701c68ba
Hannes <hannesmalter1234@gmail.com>
2023-12-07 10:43:05 +0200

    Updated

---
+++ Uus kaust/TARpe21ShopMalter/Models/Car/CarIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARpe21ShopMalter.Core.Domain;
+
+namespace TARpe21ShopMalter.Models.Car
+{
+    public class CarIndexViewModel
+    {
+        public Guid Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public bool IsUsed { get; set; }
+
+        //db only
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
